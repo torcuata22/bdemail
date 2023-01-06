f31995f2eecf0bae90c8707be8f7d1815375c507
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2023-01-06 15:23:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 import smtplib
+import smtplib
+import datetime as dt
+
+my_email = "pr0b4nd0xxy@gmail.com"
+password = "qwfdtudiwexwxvmx"
+target_email = "imthefreiby@gmail.com"
+#message = "Hello"
+
+# connection = smtplib.SMTP("smtp.gmail.com",587)
+# connection.starttls() #makes connections ecure through encryption
+# connection.login(user=my_email, password=password)
+# connection.sendmail(
+#     from_addr=my_email, 
+#     to_addrs=target_email, 
+#     msg="Subject: Hello Freiby\n\n This is the body of the email")
+# connection.close()
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user = my_email, password = password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="soyellokito768@gmail.com",
+        msg = "Subject: Hello Loki \n\n This is the body of the email"
+    )
+    
