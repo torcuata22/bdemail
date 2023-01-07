92ca99d64611b16ec961b4314681630c3fae6f9d
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-01-06 20:29:23 -0600

    middle

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import smtplib
-import smtplib
 import datetime as dt
+import random
 
 
 
-my_email = "pr0b4nd0xxy@gmail.com"
+my_email = "pr0b4nd0xxy@gmail.com" #from_email
 password = "qwfdtudiwexwxvmx"
-target_email = "imthefreiby@gmail.com"
+ #["soyellokito768@gmail.com", "imthefreiby@google.com"]
+
+email_list = ["soyellokito768@gmail.com", "imthefreiby@google.com", "pr0b4nd0xxy@gmail.com"]
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_the_week = now.weekday()
+weekday = now.weekday()
+if weekday == 4:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+
+    # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    #     connection.starttls()
+    #     connection.login(user = my_email, password = password)
+    #     connection.sendmail(
+    #     from_addr=my_email,
+    #     to_addrs="imthefreiby@gmail.com",
+    #     msg = f"Subject: Monday Motivation \n\n{quote}"
+    # )
+
+    for email in email_list:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user = my_email, password = password)
+            connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email,
+            msg = f"Subject: Monday Motivation \n\n{quote}"
+        )
+
+
